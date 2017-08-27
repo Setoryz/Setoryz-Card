3fac2fb13ef33982c49a43cec4a99f3599973075
Odukoya Jesuseyitan <Setoryz@gmail.com>
2017-08-27 17:55:00 +0100

    ui update

---
+++ Setoryz Card/Options.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace Setoryz_Card
+{
+    /// <summary>
+    /// Interaction logic for Options.xaml
+    /// </summary>
+    public partial class Options : Window
+    {
+        private GameOptions gameOptions;
+        public Options()
+        {
+            if (gameOptions == null)
+            {
+                if (File.Exists("GameOptions.xml"))
+                {
+                    using (var stream = File.OpenRead("GameOptions.xml"))
+                    {
+                        var serializer = new XmlSerializer(typeof(GameOptions));
+                        gameOptions = serializer.Deserialize(stream) as GameOptions;
+                    }
+                }
+                else
+                    gameOptions = new GameOptions();
+            }
+            DataContext = gameOptions;
+            InitializeComponent();
+        }
+
+        private void radiobtnDumbAI_Checked(object sender, RoutedEventArgs e)
+        {
+            gameOptions.ComputerSkill = ComputerSkillLevel.Dumb;
+        }
+
+        private void radiobtnGoodAI_Checked(object sender, RoutedEventArgs e)
+        {
+            gameOptions.ComputerSkill = ComputerSkillLevel.Good;
+        }
+
+        private void radiobtnCheatingAI_Checked(object sender, RoutedEventArgs e)
+        {
+            gameOptions.ComputerSkill = ComputerSkillLevel.Cheats;
+        }
+
+        private void gridGame_GotFocus(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void textBoxTimeAllowed_GotFocus(object sender, RoutedEventArgs e)
+        {
+            textBoxTimeAllowed.SelectAll();
+        }
+
+        private void textBoxTimeAllowed_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            var control = sender as TextBox;
+            if (control == null)
+                return;
+            Keyboard.Focus(control);
+            e.Handled = true;
+        }
+
+        private void btnOk_Click(object sender, RoutedEventArgs e)
+        {
+            using (var stream = File.Open("GameOptions.xml", FileMode.Create))
+            {
+                var serializer = new Xmlseerializer(typeof(GameOptions));
+                serializer.serialize(stream,GameOptions   )
+            }
+        }
+    }
+}
---
+++ Setoryz Card/images/GameOptions.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace Setoryz_Card
+{
+    [Serializable]
+    /// <summary>
+    /// GameOptions
+    /// </summary>
+    /// <param type="class"/>
+    public class GameOptions : INotifyPropertyChanged
+    {
+        private bool playAgainstComputer = true;
+        private int numberOfPlayers = 2;
+        private ComputerSkillLevel computerSkill = ComputerSkillLevel.Dumb;
+        /// <summary>
+        ///  NumberOfPlayers
+        /// </summary>
+        public int NumberOfPlayers
+        {
+            get { return numberOfPlayers; }
+            set
+            {
+                numberOfPlayers = value;
+                OnPropertyChanged(nameof (NumberOfPlayers));
+            }
+        }
+        /// <summary>
+        /// Play Against Computer chkbox
+        /// </summary>
+        public bool PlayAgainstComputer
+        {
+            get { return playAgainstComputer; }
+            set
+            {
+                playAgainstComputer = value;
+                OnPropertyChanged(nameof(PlayAgainstComputer));
+            }
+        }
+        /// <summary>
+        /// Computer Skill Level
+        /// </summary>
+        public ComputerSkillLevel ComputerSkill
+        {
+            get { return ComputerSkill; }
+            set
+            {
+                computerSkill = value;
+                OnPropertyChanged(nameof(ComputerSkill));
+            }
+        }
+        /// <summary>
+        /// Property Changed
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+        /// <summary>
+        /// On Property Changed
+        /// </summary>
+        /// <param name="propertyName"></param>
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+    [Serializable]
+    /// <summary>
+    /// Computer Skill Level
+    /// </summary>
+    public enum ComputerSkillLevel
+    {
+        Dumb,
+        Good,
+        Cheats
+    }
+}
